29bbd99cf9c71769453ea922bf8d6769314c2f4f
JQUAN CALEK <jquancollins96@gmail.com>
2022-07-06 10:19:54 -0400

    wip:added a detail page

--- main_app/views.py
+++ main_app/views.py
@@ -25,3 +25,7 @@ def cats_index(request):
   cats = Cat.objects.all()
   return render(request, 'cats/index.html', { 'cats': cats })
 
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', { 'cat': cat })
+
